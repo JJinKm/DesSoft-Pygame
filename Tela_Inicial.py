16da5880b5b88268bc52cef45b11fb199a4f6a25
Josh Kim <joshuajk@al.inpser.edu.br>
2023-11-13 11:33:42 -0300

    Movimento do boneco

--- Tela_Inicial.py
+++ Tela_Inicial.py
@@ -6,12 +6,66 @@ HEIGHT = 600
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Nome')
 
+SHIP_WIDTH = 50
+SHIP_HEIGHT = 38
+ship_img = pygame.image.load('assets/img/playerShip1_orange.png').convert_alpha()
+ship_img = pygame.transform.scale(ship_img, (SHIP_WIDTH, SHIP_HEIGHT))
+
+class character(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT /2
+        self.speedx = 0
+        self.speedy = 0
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+
+clock = pygame.time.Clock()
+FPS = 30
+
 game = True
+all_sprites = pygame.sprite.Group()
+player = character(ship_img)
+all_sprites.add(player)
 while game:
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
-    window.fill((0,0,0))
-    pygame.display.update
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.speedx -= 8
+            if event.key == pygame.K_RIGHT:
+                player.speedx += 8
+            if event.key == pygame.K_UP:
+                player.speedy -= 8
+            if event.key == pygame.K_DOWN:
+                player.speedy += 8
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player.speedx += 8
+            if event.key == pygame.K_RIGHT:
+                player.speedx -= 8
+            if event.key == pygame.K_UP:
+                player.speedy += 8
+            if event.key == pygame.K_DOWN:
+                player.speedy -= 8
+    all_sprites.update()
+    window.fill((255,255,255))
+    all_sprites.draw(window)
+    pygame.display.update()
     
 pygame.quit()
